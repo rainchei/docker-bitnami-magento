3c8e4064b161f7135075923058118af9206b6ecb
forrestlin <forrest_lin@htc.com>
2020-03-15 11:50:24 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-find . -type f -name '.DS_Store' -delete
+setenv() {
+  BASE="rainchei/docker-magento"
+  BUILD_DATE="$(date '+%Y%m%d')"
+  TAG="$(git describe --dirty)}-${BUILD_DATE}"
+}
 
-BUILD_DATE="$(date '+%Y%m%d')"
+fail() {
+  echo "$1"
+  exit "${2-1}"  # Return a code specified by $2 or 1 by default.
+}
 
-BASE="rainchei/docker-magento"
+main() {
+  cd $(dirname $0)
+  find . -type f -name '.DS_Store' -delete
+  setenv
 
-TAG="${GIT_COMMIT:-$(git describe --long --dirty --abbrev=10 --tags --always)}-${BUILD_DATE}"
+  # you must commit changes first
+  git diff --cached --exit-code || exit 1
+  git diff --exit-code || exit 2
 
-# you must commit changes first
-git diff --cached --exit-code || exit 1
-git diff --exit-code || exit 2
+  echo "== Build Start."
+  start=$(date +%s)
 
-DOCKER_BUILDKIT=1 docker build --pull -t "${BASE}:${TAG}" .
+  DOCKER_BUILDKIT=1 docker build --pull -t "${BASE}:${TAG}" . \
+  || fail "Failed to build ${BASE}:${TAG}." 2
 
-STATUS=$?
+  end=$(date +%s)
+  echo "== Build End."
+  echo
+  echo "It costs $((end-start)) seconds."
+  echo
+  docker push "${BASE}:${TAG}" \
+  || fail "Failed to push ${BASE}:${TAG}." 2
 
-# If build was successful, then push to docker registry
-if [ $STATUS -eq 0 ]; then
-  docker push "${BASE}:${TAG}"
-else
-  exit $STATUS
-fi
+  echo "${BASE}:${TAG}"
+}
 
-echo "${BASE}:${TAG}"
+## ================================================
 
-
-
+main
