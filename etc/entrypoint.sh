95e419a282f5403faa2b981998cd971820a43059
forrestlin <forrest_lin@htc.com>
2020-03-15 21:41:18 +0800

    update entrypoint

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-service php7.2-fpm start \
-&& service nginx start
+
+main() {
+  PHPFPM_LOG='/var/log/php7.2-fpm.log'
+  echo "Starting php-fpm."
+  service php7.2-fpm start
+
+  SOCK='/run/php/php7.2-fpm.sock'
+  WAIT='5'
+  while [ ! -S ${SOCK} ]
+  do
+    echo "${SOCK} does not exist! Retry in ${WAIT}s."
+    sleep ${WAIT}
+  done
+
+  service nginx start
+  [ -f ${PHP_LOG} ] && tail -f ${PHP_LOG}
+}
+
+# ===
+
+main
