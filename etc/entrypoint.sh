004b2fab10b9c4df635ec87fb79dd4e7e487cc52
forrestlin <forrest_lin@htc.com>
2020-03-16 12:06:44 +0800

    entrypoint

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -3,6 +3,7 @@
 setenv() {
   MAGENTO_ROOT='/var/www/magento2'
   MAGENTO_USER='gosu www-data:www-data'
+  MAGENTO_ENV='/etc/magento2/app/etc/env.php'
   PHPFPM_LOG='/var/log/php7.2-fpm.log'
   NGINX_ERR_LOG='/var/log/nginx/error.log'
   SOCK='/run/php/php7.2-fpm.sock'
@@ -11,6 +12,10 @@ setenv() {
 
 main() {
   setenv
+
+  # TODO: dockerize env.php
+  echo "Setting up env.php for magento2."
+  ${MAGENTO_USER} cp ${MAGENTO_ENV} ${MAGENTO_ROOT}/app/etc/env.php
 
   echo "Starting php-fpm."
   service php7.2-fpm start
