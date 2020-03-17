86b80bba81ef02a83126ce80749a2b253fbb98ed
forrestlin <forrest_lin@htc.com>
2020-03-17 20:48:52 +0800

    nginx entrypoint

--- nginx/etc/entrypoint.sh
+++ nginx/etc/entrypoint.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+set -eo pipefail
 
-PHPFPM_SOCK='/var/run/php/php7.2-fpm.sock'
-WAIT='5'
+setenv() {
+  PHPFPM_SOCK='/var/run/php/php7.2-fpm.sock'
+  WAIT='5'
+}
 
-while [ ! -S ${PHPFPM_SOCK} ]; do
-  echo "${PHPFPM_SOCK} not found! Retry in ${WAIT}s." >&2
-  sleep ${WAIT}
-done
+main() {
+  setenv
 
-echo "Starting nginx."
-exec /usr/sbin/nginx -g 'daemon off;'
+  while [ ! -S ${PHPFPM_SOCK} ]; do
+    echo "${PHPFPM_SOCK} not found! Retry in ${WAIT}s." >&2
+    sleep ${WAIT}
+  done
+
+  echo "Starting nginx."
+  exec /usr/sbin/nginx -g 'daemon off;'
+}
+
+# ===
+
+main
