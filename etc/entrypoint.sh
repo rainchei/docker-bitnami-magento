beb070fba0c4337f6d931c81807e08504ca792d3
forrestlin <forrest_lin@htc.com>
2020-03-17 20:46:47 +0800

    entrypoint

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+set -eo pipefail
 
-MAGENTO_ROOT='/var/www/magento2'
-MAGENTO_USER='gosu www-data:www-data'
-MAGENTO_ENV='/etc/magento2/app/etc/env.php'
-PHPFPM_LOG='/var/log/php7.2-fpm.log'
+setenv() {
+  MAGENTO_ROOT='/var/www/magento2'
+  MAGENTO_USER='gosu www-data:www-data'
+  MAGENTO_ENV='/etc/magento2/app/etc/env.php'
+  PHPFPM_LOG='/var/log/php7.2-fpm.log'
+}
 
-# TODO: dockerize env.php
-echo "Setting up env.php for magento2."
-${MAGENTO_USER} cp ${MAGENTO_ENV} ${MAGENTO_ROOT}/app/etc/env.php
+main() {
+  setenv
 
-echo "Starting php-fpm."
-exec /usr/sbin/php-fpm7.2
+  # TODO: dockerize env.php
+  echo "Setting up env.php for magento2."
+  ${MAGENTO_USER} cp ${MAGENTO_ENV} ${MAGENTO_ROOT}/app/etc/env.php
+
+  echo "Starting php-fpm."
+  exec /usr/sbin/php-fpm7.2
+  tail -f ${PHPFPM_LOG}
+}
+
+# ===
+
+main
