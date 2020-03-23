1bbbe3d40bf12ab23b67164338ff86d73af0fbfc
forrestlin <forrest_lin@htc.com>
2020-03-23 11:45:23 +0800

    moving files around

---
+++ etc/php/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+setenv() {
+  BASE="rainchei/docker-magento-phpfpm"
+  BUILD_DATE="$(date '+%Y%m%d')"
+  TAG="$(git describe --dirty)-${BUILD_DATE}"
+}
+
+fail() {
+  echo "$1"
+  exit "${2-1}"  # Return a code specified by $2 or 1 by default.
+}
+
+main() {
+  cd $(dirname $0)
+  find . -type f -name '.DS_Store' -delete
+  setenv
+
+  # you must commit changes first
+  git diff --cached --exit-code || exit 1
+  git diff --exit-code || exit 2
+
+  echo "== Build Start."
+  start=$(date +%s)
+
+  DOCKER_BUILDKIT=1 docker build \
+    --pull \
+    -t "${BASE}:${TAG}" \
+    . \
+  || fail "Failed to build ${BASE}:${TAG}." 2
+
+  end=$(date +%s)
+  echo "== Build End."
+  echo
+  echo "It costs $((end-start)) seconds."
+  echo
+  docker push "${BASE}:${TAG}" \
+  || fail "Failed to push ${BASE}:${TAG}." 2
+
+  echo "${BASE}:${TAG}"
+}
+
+## ================================================
+
+main
