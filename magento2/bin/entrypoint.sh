593657bb593d9ece2b037271246fd4cae7117cce
forrestlin <forrest_lin@htc.com>
2020-03-24 15:04:36 +0800

    update entrypoint

--- magento2/bin/entrypoint.sh
+++ magento2/bin/entrypoint.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 
-setenv() {
-  MAGENTO_USER='gosu www-data:www-data'
-  MAGENTO_ENVPHP='/etc/magento2/app/etc/env.php'
-}
-
 main() {
   cd $(dirname $0)
-  setenv
-
-  # TODO: dockerize env.php
-  echo "Setting up env.php for magento2."
-  ${MAGENTO_USER} cp ${MAGENTO_ENVPHP} ./app/etc/env.php
 
   echo "Starting php-fpm."
   exec /usr/sbin/php-fpm7.2
